d45d2f591ba8d7e33f268c1941f07ba3f4fd77aa
gersonc <gcarrasco.hdc@gmail.com>
2018-10-06 16:04:49 -0500

    Add validations login

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,10 @@ class _LoginState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
   bool _obscureText = true;
   bool _visibilityPassword = false;
-
+  bool _autoValidate = false;
+  // VARIABLES
+  String _email;
+  String _password;
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -63,83 +66,136 @@ class _LoginState extends State<LoginPage> {
             tileMode:TileMode.clamp
           ),
         ),
-        child: new Form(
-          key: _formKey,
-          autovalidate: true,
-          child: new ListView(
-            padding: new EdgeInsets.fromLTRB(40.0, 100.0, 40.0, 0.0),
-            children: <Widget>[
-              // new Image(
-              //   image: new AssetImage("assets/images/book.png"),
-              // ),
-
-              new Container(
-                // padding: new EdgeInsets.all(15.0),
-                child: new TextFormField(
-                  decoration: new InputDecoration(
-                    icon: new Icon(Icons.person,color: toptionalColor),
-                    hintText: 'Email',
-                    // contentPadding: const EdgeInsets.all(18.0),
-                    // hintStyle: TextStyle(color:Colors.grey),
-                  ),
-                  keyboardType: TextInputType.emailAddress
-                )
+        child:new Container(
+          alignment: Alignment.topCenter,
+            padding: new EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * .2,
+                right: 20.0,
+                left: 20.0),
+            child: new Container(
+              height: MediaQuery.of(context).size.height* .6,
+              width: MediaQuery.of(context).size.width,
+              child: new Card(
+                child: formUI(),
+                color: Colors.blue,
+                elevation: 4.0,
               ),
-
-              new Container(
-                // padding: new EdgeInsets.all(15.0),
-                child: new TextFormField(
-                  decoration: new InputDecoration(
-                    icon: new Icon(Icons.lock,color:toptionalColor),
-                    suffixIcon: IconButton(
-                      icon: _visibilityPassword? Icon(Icons.visibility_off): Icon(Icons.visibility),
-                      onPressed:(){
-                        setState(() {
-                          _visibilityPassword = !_visibilityPassword;
-                          _obscureText = !_obscureText;
-                        });
-                      } ,
-                    ),
-                    hintText: 'Password',
-                    hintStyle: TextStyle(color:Colors.white)
-                  ),
-                  validator: (val) => val.length<6 ? "Password too short" : null,
-                  obscureText: _obscureText,
-                )
-              ),
-              
-              new Container(
-                // padding: new EdgeInsets.only(left:60.0,right: 60.0),
-                child: 
-                  new RaisedButton(
-                    child: new Text('INGRESAR',style:new TextStyle(color:tbackgroundColor,fontSize: 20.0)),
-                    padding: EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 15.0),
-                    onPressed: (){
-                      Navigator.of(context).pushNamed('/mainmap');
-                    },
-                    // shape: new RoundedRectangleBorder(borderRadius:new BorderRadius.circular(20.0)),
-                  ),
-                margin: EdgeInsets.only(top: 20.0),
-                
-              ),
-              new Container(
-                // padding: new EdgeInsets.only(left:60.0,right: 60.0),
-                child: 
-                  new RaisedButton(
-                    child: new Text('REGÍSTRATE',style:new TextStyle(color:tbackgroundColor,fontSize: 20.0)),
-                    padding: EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 15.0),
-                    onPressed: () {
-                      Navigator.of(context).pushNamed('/register');
-                    },
-                    // shape: new RoundedRectangleBorder(borderRadius:new BorderRadius.circular(20.0)),
-                  ),
-                margin: EdgeInsets.only(top: 20.0),
-              ),
-            ]
-          ),
+            ),
         ),
 	    )
     );
+  }
+
+
+  Widget formUI(){
+    return new Form(
+      key: _formKey,
+      autovalidate: _autoValidate,
+      child: new ListView(
+        padding: new EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
+        children: <Widget>[
+
+          new Container(
+            child: new TextFormField(
+              decoration: new InputDecoration(
+                labelText: 'Email',
+                labelStyle: new TextStyle( color: Colors.white),
+                icon: new Icon(Icons.person,color: toptionalColor)
+              ),
+              keyboardType: TextInputType.emailAddress,
+              validator: validateEmail,
+              onSaved: (String val){
+                _email = val;
+              },
+            )
+          ),
+
+          new Container(
+
+            child: new TextFormField(
+              decoration: new InputDecoration(
+                labelText: 'Password',
+                labelStyle: TextStyle(color:Colors.white),
+                icon: new Icon(Icons.lock,color:toptionalColor),
+                suffixIcon: IconButton(
+                  icon: _visibilityPassword? Icon(Icons.visibility_off): Icon(Icons.visibility),
+                  onPressed:(){
+                    setState(() {
+                      _visibilityPassword = !_visibilityPassword;
+                      _obscureText = !_obscureText;
+                    });
+                  } ,
+                ),
+              ),
+              obscureText: _obscureText,
+              validator: validatePassword,
+              onSaved: (String val){
+                _password = val;
+              }
+            )
+          ),
+          
+          new Container(
+            child: 
+              new RaisedButton(
+                child: new Text('INGRESAR',style:new TextStyle(color:tbackgroundColor,fontSize: 20.0)),
+                padding: EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 15.0),
+                onPressed: _validateInputs
+              ),
+            margin: EdgeInsets.only(top: 20.0),
+            
+          ),
+          new Container(
+            
+            child: 
+              new RaisedButton(
+                child: new Text('REGÍSTRATE',style:new TextStyle(color:tbackgroundColor,fontSize: 20.0)),
+                padding: EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 15.0),
+                onPressed: (){
+                  Navigator.of(context).pushNamed('/register');
+                }
+              ),
+            margin: EdgeInsets.only(top: 20.0),
+          ),
+        ]
+      ),
+    );
+  }
+
+  String validateEmail(String value){
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(value))
+      return 'Ingresar un email válido';
+    else
+      return null;
+  }
+
+  String validatePassword(String value){
+    if(value.length<6)
+      return "Password too short";
+    else
+      return null;  
+  }
+
+  void _validateInputs() {
+    if (_formKey.currentState.validate()) {
+  //    If all data are correct then save data to out variables
+      // RNavigator.of(context).pushNamed('/register');
+      Navigator.of(context).pushNamed('/mainmap');
+      print("Validado");
+      _formKey.currentState.save();
+    } else {
+  //    If all data are not valid then start auto validation.
+      // print('No validado');
+      // Scaffold.of(context).showSnackBar(new SnackBar(
+      //   content:new Text("No validado")
+      // ));
+      setState(() {
+        _autoValidate = true;
+      });
+    }
   }
 }
 
--- lib/viewmap.dart
+++ lib/viewmap.dart
@@ -1,5 +1,6 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 // var myKey = 'AIzaSyAohyz0ihWkHkPgQXCk1iNr72QHol0uSbQ';
 class MainMapPage extends StatefulWidget{
@@ -8,44 +9,62 @@ class MainMapPage extends StatefulWidget{
 }
 
 class _MainMapPageSate extends State<MainMapPage>{
+  final TextEditingController _filter = new TextEditingController();
+  final dio = new Dio();
+  String _searchText = "";
+  List names = new List();
+  List filteredNames = new List();
+  Icon _searchIcon = new Icon(Icons.search);
+  Widget _appTitleBar = new Text('Search');
+  
   GoogleMapController mapController;
-
-  // displayMap(){
-  //   mapView.show(new MapOptions(
-  //     mapViewType: MapViewType.normal,
-  //     initialCameraPosition: new CameraPosition(
-  //       new Location(35.22, -101.83), 15.0),
-  //     showUserLocation: true,
-  //     title: 'Google Map'
-  //   ));
-  // }
   @override
   Widget build(BuildContext context) {
-    return new Stack(
-      children: <Widget>[
-        new GoogleMap(
-          onMapCreated:_onMapCreated
-        ),
-        new Container(
-          alignment: Alignment.topCenter,
-          padding: new EdgeInsets.only(
-              top: MediaQuery.of(context).size.height * .58,
-              right: 20.0,
-              left: 20.0),
-          child: new Container(
-            height: 200.0,
-            width: MediaQuery.of(context).size.width,
-            child: new Card(
-              color: Colors.white,
-              elevation: 4.0,
-            ),
+    return new Scaffold(
+      appBar: new AppBar(),
+      body: new Stack(
+          children: <Widget>[
+          new GoogleMap(
+            onMapCreated:_onMapCreated
           ),
-        )
-      ],
+          new Container(
+            alignment: Alignment.topCenter,
+            padding: new EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * .5,
+                right: 20.0,
+                left: 20.0),
+            child: new Container(
+              height: 80.0,
+              width: MediaQuery.of(context).size.width,
+              child: new Card(
+                child: new TextField(),
+                color: Colors.red,
+                elevation: 4.0,
+              ),
+            ),
+          )
+        ],
+      )
     );
   }
 
+  /* ///////////////FUNCTIONS MAPS/////////////// */
   void _onMapCreated(GoogleMapController controller) {
     setState(() { mapController = controller; });
+  }
+
+  /* ///////////////FUNCTIONS SEARCH BAR/////////////// */
+  void _getNamesApi() async{
+    final response = await dio.get('https://swapi.co/api/people');
+    List tempList = new List();
+
+    for (var i = 0; i < response.data['results'] ; i++) {
+      tempList.add(response.data['results'][i]);
+    }
+
+    setState(() {
+          names = tempList;
+          filteredNames = names;
+        });
   }
 }
